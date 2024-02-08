fe62c2446de096708b3f5837d5021cc683e82e4e
eliasyeme <eliasyeme@gmail.com>
2024-02-08 14:10:48 +0300

    Add status indecator

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  const url = `http://${window.location.hostname}:5001/api/v1/status/`;
+  $.get(url, function (data, status) {
+    if (status === 'success') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
+  const amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenities[$(this).attr('data-id')];
+    }
+    const names = Object.values(amenities);
+    if (names.length > 0) {
+      $('.amenities h4').text(names.join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp;');
+    }
+  });
+});
